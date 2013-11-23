d73b228c18bc0b731ad214d25e1f2a3820b6231c
astux7 <Asta.bevainyte@gmail.com>
2013-11-23 12:15:38 +0000

    simplify some code places

--- euler1.rb
+++ euler1.rb
@@ -8,11 +8,9 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 def sum_of_multiples_3_5(range_end)
 	sum = 0
 	1.upto(range_end) do |n| 
-		if n % 3 == 0 || n % 5 == 0 
-		   sum+= n
-		end
+		sum+= n if n % 3 == 0 || n % 5 == 0 
 	end
-	return sum
+    sum
 end
 
 print "Sum of multiples of 3 and 5 till 1000 is ",sum_of_multiples_3_5(999), "\n"
